256aaffeadafc84fb62ef23211fad201ed0a3769
DESKTOP-2RSDEG8\User <dingshengho@cspeed.com.my>
2023-08-07 23:24:58 +0800

    adminkit setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import "./bootstrap";
+
+import "./modules/bootstrap";
+import "./modules/sidebar";
+import "./modules/theme";
+import "./modules/feather";
+
+// Charts
+import "./modules/chartjs";
+
+// Forms
+import "./modules/flatpickr";
+
+// Maps
+import "./modules/vector-maps";
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        host: "business-card.test",
+        watch: {
+            usePolling: true,
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // Additional SCSS options if needed
+            },
+        },
+    },
 });
